f0eb1c45cb5d8d722a376eadefa84a9d1889548f
oxoxoox <72512841+oxoxoox@users.noreply.github.com>
2021-11-22 22:53:02 +0800

    收藏功能的实现

--- 简化版前端与后端结合/pages/i1/i1.js
+++ 简化版前端与后端结合/pages/i1/i1.js
@@ -1,66 +1,35 @@
-// pages/i1/i1.js
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
+data: {
+list:'',
+lang:'',
+since:'',
+toastText:''
+},
+houduan: function () {
+  var that = this;
+  wx.request({
+  url: "https://trendings.herokuapp.com/repo?lang=java&since=weekly",
+  method: 'get',
+  header: {
+  'content-type': 'application/json' // 默认值
+  },
+  success: function (res) {
+  console.log(res.data)//打印到控制台
+  
+  if (res.data == null) {
+  var toastText = '数据获取失败';
+  wx.showToast({
+  title: toastText,
+  icon: '',
+  duration: 2000
+  });
+  } else {
+  that.setData({
+    list:res.data.items
+  })
+  }
+  }
+  })
+}
 })
