13b92fecc651c50eeeb994ecf4d25ce4d079984d
yinyuxuan2 <60737967+yinyuxuan2@users.noreply.github.com>
2021-11-22 22:22:56 +0800

    Add files via upload

---
+++ 简化版前端与后端结合/app.js
@@ -0,0 +1,28 @@
+// app.js
+App({
+  globalData: {
+    userInfo: null,
+    user:'',
+    repo:null,
+    name:'',
+    list:'',
+    path:'',
+    isdir:''
+  },
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
