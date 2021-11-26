d9f15431fb1d21b8967410d423d874b2e35d2e55
BuildStar12138 <90128204+BuildStar12138@users.noreply.github.com>
2021-11-26 22:32:58 +0800

    Update index.js

--- cloudfunctions/search7/index.js
+++ cloudfunctions/search7/index.js
@@ -2,7 +2,7 @@ const got = require('got')
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const  response = await got("https://api.github.com/users/"+event.userT)
+  const  response = await got( "https://api.github.com/search/repositories?q="+event.inputVal)
   return response.body
 }
 
