4dac10167dc3e3d1a991bff3e660ac1151584eda
yinyuxuan2 <60737967+yinyuxuan2@users.noreply.github.com>
2021-11-26 21:54:42 +0800

    Delete cloudfunctions directory

--- cloudfunctions/search5/index.js
+++
@@ -1,8 +0,0 @@
-const got = require('got')
-
-// 云函数入口函数
-exports.main = async (event, context) => {
-  const  response = await got( "https://api.github.com/repos/"+event.name)
-  return response.body
-}
-
--- cloudfunctions/search7/index.js
+++
@@ -1,8 +0,0 @@
-const got = require('got')
-
-// 云函数入口函数
-exports.main = async (event, context) => {
-  const  response = await got( "https://api.github.com/search/repositories?q="+event.inputVal)
-  return response.body
-}
-
