e8c75d943fe7e2a5399c0134f99baec6faa15512
Jim031902101 <60083091+Jim031902101@users.noreply.github.com>
2021-11-26 22:16:26 +0800

    Update index.js

--- cloudfunctions/search3/index.js
+++ cloudfunctions/search3/index.js
@@ -2,7 +2,6 @@ const got = require('got')
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const  response = await got("https://api.github.com/users/"+event.userT)
+  const  response = await got("https://api.github.com/users/"+event.userT+"/repos")
   return response.body
 }
-
